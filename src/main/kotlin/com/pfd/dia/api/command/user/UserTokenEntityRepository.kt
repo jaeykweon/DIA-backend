2f0dc50d61184edecf21811fb621a9a4717783a4
jay <jaeykweon@naver.com>
2022-08-30 00:33:27 +0900

    bugfix: delete fault repo

--- src/main/kotlin/com/pfd/dia/api/command/user/UserTokenEntityRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.pfd.dia.api.command.user
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserTokenEntityRepository: JpaRepository<UserTokenEntityRepository, Long> {
-}
